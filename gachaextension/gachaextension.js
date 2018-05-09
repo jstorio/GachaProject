f2d29f9c7de723cf3b0cd79e997222ede360401d
James Torio <32937709+jstorio@users.noreply.github.com>
2018-05-09 12:56:23 -0400

    added onCreatedNavigationTarget
    Understood how onBeforeNavigate and onCreatedNavigationTarget works. Implemented and working fine as expected.
    
    Will test out browserAction API on the next commit.

--- gachaextension/gachaextension.js
+++ gachaextension/gachaextension.js
@@ -3,12 +3,19 @@
 //placeholder for local currency
 var currency = 0;
 
-//adds currency for every triggered even
-function addCurrency () {
+function addCurrencyOnBeforeNavigate () {
 	currency = currency + 1;
 	window.alert(currency + " - Trigger: webNavigation.onBeforeNavigate");
 };
 
-//*****TRIGGERS*****//
+function addCurrencyOnCreatedNavigationTarget () {
+	currency = currency + 1;
+	window.alert(currency + " - Trigger: webNavigation.onCreatedNavigationTarget");
+};
 
-browser.webNavigation.onBeforeNavigate.addListener (addCurrency);
+//*****TRIGGERS/LISTENERS*****//
+
+//triggers when navigating to another web page
+browser.webNavigation.onBeforeNavigate.addListener (addCurrencyOnBeforeNavigate);
+//triggers when user opens a web page via new tab/new window (also triggers onBeforeNavigate)
+browser.webNavigation.onCreatedNavigationTarget.addListener(addCurrencyOnCreatedNavigationTarget);
