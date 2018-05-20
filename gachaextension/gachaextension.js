cf3cfa84b1ddbc31cdfc74ccfe392ec900a61d27
James Torio <32937709+jstorio@users.noreply.github.com>
2018-05-20 17:43:57 -0400

    Add files via upload

--- gachaextension/gachaextension.js
+++ gachaextension/gachaextension.js
@@ -2,10 +2,15 @@
 
 //placeholder for local currency
 var currency = 0;
+var fs = require('fs');
+
+var jsonCurrencyCont = fs.readFileSync("currency.json");
+var jsonCurrency = JSON.parse(jsonCurrencyCont);
 
 function addCurrencyOnBeforeNavigate () {
-	currency = currency + 1;
-	window.alert(currency + " - Trigger: webNavigation.onBeforeNavigate");
+
+	jsonCurrency.currency = jsonCurrency + 1;
+	window.alert(jsonCurrency.currency + " - Trigger: webNavigation.onBeforeNavigate");
 };
 
 function addCurrencyOnCreatedNavigationTarget () {
@@ -13,9 +18,19 @@ function addCurrencyOnCreatedNavigationTarget () {
 	window.alert(currency + " - Trigger: webNavigation.onCreatedNavigationTarget");
 };
 
+function openPage() {
+	browser.tabs.create({
+		url: "popups/gachapopup.html"
+});
+}
+
 //*****TRIGGERS/LISTENERS*****//
+
+browser.browserAction.onClicked.addListener(openPage);
 
 //triggers when navigating to another web page
 browser.webNavigation.onBeforeNavigate.addListener (addCurrencyOnBeforeNavigate);
 //triggers when user opens a web page via new tab/new window (also triggers onBeforeNavigate)
 browser.webNavigation.onCreatedNavigationTarget.addListener(addCurrencyOnCreatedNavigationTarget);
+
+browser.webNavigation.onCommitted.addListener (addCurrencyOnBeforeNavigate);
